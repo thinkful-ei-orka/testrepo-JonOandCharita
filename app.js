2f2850348c53037dc0c8d6668ba24974f618f524
Charita Nichols <nichols.charita@gmail.com>
2020-04-20 17:18:49 -0400

    added new function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+function getYearOfBirth(age){
+    return 2020 - age;
+}
 function createGreeting(name, age){
-    const yearOfBirth = 2020 - age;
+    const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
