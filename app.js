a2e587e597122e113298cc56e63a68ace74eeeab
Jon <jdog992@gmail.com>
2020-04-20 17:13:18 -0400

    added yearOfBirth variable

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function createGreeting(name, age){
-    return `Hi, my name is ${name} and I'm ${age} years old`;
+    const yearOfBirth = 2020 - age;
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting();
